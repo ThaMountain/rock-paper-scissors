97fb817d2bb323ef9d05efe364eba7ef23ff1186
Nick Deloach <nickdeloach@yahoo.com>
2022-06-22 23:05:50 -0400

    begin building structure of rockPaperScissors game

--- files/index.js
+++ files/index.js
@@ -1,3 +1,23 @@
+let choiceArray = ["rock", "paper", "scissors"];
 function computerPlay() {
-let i = Math.random()
-};
+  return choiceArray[~~(Math.random() * choiceArray.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  const compareObj = {
+    // Object {Key is what you're checking : value is the thing that beats it}
+    rock: "paper",
+    paper: "scissors",
+    scissors: "rock",
+  };
+  computerSelection = computerPlay(); //assings variable to return value of computerPlay()
+  playerSelection = playerSelection.toLowerCase();
+  if (compareObj[playerSelection] == computerSelection) {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+  } else if (compareObj[computerSelection] == playerSelection) {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  } else {
+    console.log("Tie!");
+  }
+}
+playRound("rock");
