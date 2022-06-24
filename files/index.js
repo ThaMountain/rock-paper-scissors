34aeeae345759fbfabf630141801ff678c27ab45
Nick Deloach <nickdeloach@yahoo.com>
2022-06-23 23:01:18 -0400

    update index.js

--- files/index.js
+++ files/index.js
@@ -1,9 +1,12 @@
-let choiceArray = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
+  let choiceArray = ["rock", "paper", "scissors"];
   return choiceArray[~~(Math.random() * choiceArray.length)];
 }
-
 function playRound(playerSelection, computerSelection) {
+  playerSelection = prompt("Do you want to play a game?");
   const compareObj = {
     // Object {Key is what you're checking : value is the thing that beats it}
     rock: "paper",
@@ -13,11 +16,34 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay(); //assings variable to return value of computerPlay()
   playerSelection = playerSelection.toLowerCase();
   if (compareObj[playerSelection] == computerSelection) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+    // checks if key's value is equal to computerSelection.
+    computerScore++; //adds 1 to computerScore
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`); //displays You Lose! computerSelection beats playerSelection
   } else if (compareObj[computerSelection] == playerSelection) {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    //checks if key's value is equal to playerSelection
+    playerScore++; //adds 1 to player score
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`); //displays You Win! playerSelection beats computerSelection
   } else {
     console.log("Tie!");
   }
 }
-playRound("rock");
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log(`Round ${i + 1}, Fight!`);
+    playRound();
+    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
+  }
+  if (computerScore < playerScore) {
+    console.log("Winner winner chicken dinner!")
+  }
+  else if (computerScore > playerScore) {
+    console.log("Loser loser chicken...loser.")
+  }
+  else {
+    console.log("Tie game!")
+  }
+}
+
+game();
+
